a8db87e698834aa03d3f1dd4d861bed14230274f
prakash-rahangdale <prakash170817@gmail.com>
2021-04-05 17:41:01 +0530

    implementing an approval card

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './commentdetail';
+import ApproveCard from './ApproveCard'
 
 //react props system 
 //passing and receiving props
@@ -9,11 +10,16 @@ import CommentDetail from './commentdetail';
 
 const App = () => {
     return (
-        <div className="comment container">
-         <CommentDetail author="sem" timeAgo="today at 02.00 pm" comment="nice post" avatar={faker.image.avatar()} />   
-         <CommentDetail author="alex" timeAgo="today at 04.00 pm" comment="nice blog post" avatar={faker.image.avatar()} />   
-         <CommentDetail author="jane" timeAgo="yesterday at 06.00 pm" comment="nice one" avatar={faker.image.avatar()} />   
-         <CommentDetail author="roy" timeAgo="yesterday at 08.00 pm" comment="good post" avatar={faker.image.avatar()} />   
+        <div className="ui container comment ">
+        <ApproveCard>
+         <CommentDetail author="sem" timeAgo="today at 02.00 pm" comment="nice post" avatar={faker.image.avatar()} /> 
+         </ApproveCard> 
+         <ApproveCard>
+         <CommentDetail author="alex" timeAgo="today at 04.00 pm" comment="nice blog post" avatar={faker.image.avatar()} /> </ApproveCard>   
+         <ApproveCard>
+         <CommentDetail author="jane" timeAgo="yesterday at 06.00 pm" comment="nice one" avatar={faker.image.avatar()} /> </ApproveCard>  
+         <ApproveCard>
+         <CommentDetail author="roy" timeAgo="yesterday at 08.00 pm" comment="good post" avatar={faker.image.avatar()} /> </ApproveCard>  
         </div>
     );
 };
