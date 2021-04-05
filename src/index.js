3636fcaf54977133a91f57afb0c8809e84e04734
prakash-rahangdale <prakash170817@gmail.com>
2021-04-05 13:24:41 +0530

    component nesting

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './commentdetail';
 
 const App = () => {
     return (
         <div className="comment container">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        john
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6 p.m</span>
-                    </div>
-                    <div clasname="text">Nice post!</div>
-                </div>
-            </div>
+         <CommentDetail />   
+         <CommentDetail />   
+         <CommentDetail />   
+         <CommentDetail />   
         </div>
     );
 };
